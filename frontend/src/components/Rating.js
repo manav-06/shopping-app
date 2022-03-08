0df650a472db4b60f32ae7551cc432d45831acc8
Manav Mohata <56617785+manav-06@users.noreply.github.com>
2022-03-08 16:35:58 +0530

    shorter code for ratings component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -6,28 +6,18 @@ const Rating = (props) => {
 
     return (
         <div className='rating'>
-            <span>
-                <i className={
-                    rating >= 1
-                        ? 'fas fa-star'
-                        : rating >= 0.5
-                            ? 'fas fa-star-half-alt'
-                            : 'far fa-star'
-                }
-                />
-            </span>
-            <span>
-                <i className={rating >= 2 ? 'fas fa-star' : rating >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
-            </span>
-            <span>
-                <i className={rating >= 3 ? 'fas fa-star' : rating >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
-            </span>
-            <span>
-                <i className={rating >= 4 ? 'fas fa-star' : rating >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
-            </span>
-            <span>
-                <i className={rating >= 5 ? 'fas fa-star' : rating >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
-            </span>
+            {[1, 2, 3, 4, 5].map(cur =>
+                <span>
+                    <i className={
+                        rating >= cur
+                            ? 'fas fa-star'
+                            : rating >= (cur - 0.5)
+                                ? 'fas fa-star-half-alt'
+                                : 'far fa-star'
+                    }
+                    />
+                </span>
+            )}
         </div>
     )
 }
