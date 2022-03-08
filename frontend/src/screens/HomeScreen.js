22afe62ec8e5c27820f8d9b4964df3fae0a49357
Manav Mohata <56617785+manav-06@users.noreply.github.com>
2022-03-08 19:44:09 +0530

    add product screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,8 @@ import logger from 'use-reducer-logger'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Product from '../components/Product'
+import { Helmet } from 'react-helmet-async';
+
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -45,6 +47,11 @@ const HomeScreen = () => {
 
     return (
         <div>
+            <Helmet>
+                <title>Amazona</title>
+            </Helmet>
+
+
             <h1>Featured Products</h1>
             <div className="products">
                 {
