e96d83cc1771556b2bdb364057d22a045e31def4
Manav Mohata <56617785+manav-06@users.noreply.github.com>
2022-03-19 20:21:34 +0530

    sign in ui created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { useContext } from 'react';
 import { Store } from './Store'
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SignInScreen';
 
 function App() {
 
@@ -46,6 +47,7 @@ function App() {
             <Routes>
               <Route path='/product/:slug' element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
 
